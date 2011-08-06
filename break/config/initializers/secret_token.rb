a306765dc7d4fe79ae7c6132842c9c6e76318e81
Fumiaki Sakamoto <ua6ta6@bma.biglobe.ne.jp>
2011-08-06 18:07:35 +0900

    first implemet

---
+++ break/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Break::Application.config.secret_token = 'ce5de264bcbbdebc52ff4ecd0b20648b0b17de65054f10b1f910621bfb2a17901099a30bafc5cc34ef07446e3410acd4a35504e24762f29dbc9552f2c4843d4f'
