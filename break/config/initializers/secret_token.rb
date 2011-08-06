169f4cb62ce30b46b8f3dd8b33406b977fd85b26
Fumiaki Sakamoto <ua6ta6@bma.biglobe.ne.jp>
2011-08-06 19:03:53 +0900

    third commit

--- break/config/initializers/secret_token.rb
+++ break/config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
+<<<<<<< .merge_file_TYsw0F
 Break::Application.config.secret_token = 'ce5de264bcbbdebc52ff4ecd0b20648b0b17de65054f10b1f910621bfb2a17901099a30bafc5cc34ef07446e3410acd4a35504e24762f29dbc9552f2c4843d4f'
+=======
+Break::Application.config.secret_token = '2f8891c0368ac769202cdb793d9de48090234a334b0097d3a6a9d55dd2cfb3f5f30722a6e558a3de07961ed587d3a3af32751f8b10bda7ff70ea820f9867f9c4'
+>>>>>>> .merge_file_CoVFMA
